301a88d81022e59f9e3fa5d608bb79d5741b1a4c
RoyCoding8 <royshashwata8@gmail.com>
2023-06-22 23:19:20 +0530

    Update Output.py

--- Output.py
+++ Output.py
@@ -3,5 +3,4 @@ from Gen_input import *
 
 def Output(Asg:set[tuple[worker,task]]):
     for i in Asg:
-        # print('"'+str(worker_tag[i[0].ind])+'"','should be given','"'+str(task_tag[i[1].ind])+'"')
-        print(i[0].ind,i[1].ind)
+        print('"'+str(worker_tag[i[0].ind])+'"','should be given','"'+str(task_tag[i[1].ind])+'"')
